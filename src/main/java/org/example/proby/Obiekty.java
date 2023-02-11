a189edaf07ca37d5828e21fc909861fd20e74f22
Razz <pawlowski.a.m@gmail.com>
2023-02-11 10:25:00 -0500

    first commit

---
+++ src/main/java/org/example/proby/Obiekty.java
@@ -0,0 +1,24 @@
+package org.example.proby;
+
+public class Obiekty{
+
+    public class Obj  {
+        private static String desc = "Janusz Maj";
+        private static Integer dl = 5;
+        private static Integer wys = 9;
+
+    private static PageObject obj = new PageObject(desc,dl,wys);
+    public static PageObject get(){ return obj;}
+    }
+
+    public class Obj1 {
+        private static String desc = "Janek Wisniewski";
+        private static Integer dl = 10;
+        private static Integer wys = 20;
+
+        private static PageObject obj1 = new PageObject(desc,dl,wys);
+        public static PageObject get(){ return obj1;}
+    }
+
+
+}
---
+++ src/main/java/org/example/proby/Test.java
@@ -0,0 +1,26 @@
+package org.example.proby;
+
+public class Test {
+
+
+
+    private static void metoda(Obiekty o){
+        System.out.println(Obiekty.Obj.get().getDesc());
+    }
+
+    public static void main (String[] a){
+
+    //chodzi o to, jak to zapisac, zebym mogl miec uniwersalna metode
+        // w ktorej bede mogl sprawdzac rzeczy ktore sa w Obj, Obj1, ale zebym mogl podawac obiekt caly, a nie robic po tam 5 parametrow
+        // w tej chwili co udalo mi sie osiagnac to to, ze moge zrobic obiekt klasy Obj albo Obj1 i podac go tam
+        //ale to wtedy ta metoda jest tylko dedykowana pod klase Obj/Obj1
+
+        //Obj Obj1 to sa inner classy w Obiekty
+        //Page Object setter getter
+
+
+
+
+    }
+
+}
